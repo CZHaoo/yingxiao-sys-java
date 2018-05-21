60500b670f5a602ae04bcafeb19c2431966ecb3d
czh <164376693@qq.com>
2018-05-21 17:43:07 +0800

    mybatis generator ok

--- console/src/main/java/com/zhijian/market/console/MarketConsoleApplication.java
+++ console/src/main/java/com/zhijian/market/console/MarketConsoleApplication.java
@@ -1,10 +1,12 @@
 package com.zhijian.market.console;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@MapperScan("com.zhijian.market.core.mapper")
 public class MarketConsoleApplication {
 
     public static void main(String[] args) {
--- console/src/main/java/com/zhijian/market/console/controller/HelloController.java
+++ console/src/main/java/com/zhijian/market/console/controller/HelloController.java
@@ -1,7 +1,14 @@
 package com.zhijian.market.console.controller;
 
+
+import com.zhijian.market.core.mapper.MenuMapper;
+import com.zhijian.market.core.pojo.Menu;
+import com.zhijian.market.core.pojo.MenuExample;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * 测试接口
@@ -9,8 +16,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Autowired
+    private MenuMapper menuMapper;
+
     @RequestMapping("/hello")
     public String hello() {
         return  "hello SpringBoot";
+    }
+
+    @RequestMapping("/list")
+    public List<Menu> menuList() {
+        MenuExample menuExample = new MenuExample();
+        List<Menu> menus = menuMapper.selectByExample(menuExample);
+        return menus;
     }
 }
