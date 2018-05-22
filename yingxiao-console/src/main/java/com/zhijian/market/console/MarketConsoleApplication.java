47d4dffa60919078f19dcd0cd344bdae7528258d
czh <164376693@qq.com>
2018-05-22 16:25:56 +0800

    后台接口 修复时间返回由字符串->毫秒值

--- yingxiao-console/src/main/java/com/zhijian/market/console/MarketConsoleApplication.java
+++ yingxiao-console/src/main/java/com/zhijian/market/console/MarketConsoleApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MarketConsoleApplication {
 
     public static void main(String[] args) {
+        System.out.println("启动成功");
         SpringApplication.run(MarketConsoleApplication.class, args);
     }
 
--- yingxiao-portal/src/main/java/com/zhijian/market/portal/controller/HelloController.java
+++ yingxiao-portal/src/main/java/com/zhijian/market/portal/controller/HelloController.java
@@ -1,7 +1,10 @@
 package com.zhijian.market.portal.controller;
 
+import lombok.Data;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 /**
  * 测试接口
@@ -13,7 +16,17 @@ public class HelloController {
     public String hello() {
         return  "hello SpringBoot";
     }
+    @RequestMapping("/person")
+    public Person person() {
+        Person p = new Person();
+        p.setBorn(new Date());
+        return p ;
+    }
+
+    @Data
+    class Person {
+        Date born;
 
 
-
+    }
 }
